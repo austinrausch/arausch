1d1ca8c940861fba78b1a55b4e2169d053cebdf0
Austin <akrbm4@gmail.com>
2019-10-23 23:37:21 -0400

    fix store/animation issue in unit tests

--- src/app/interests/interests.component.spec.ts
+++ src/app/interests/interests.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InterestsComponent } from './interests.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('InterestsComponent', () => {
   let component: InterestsComponent;
@@ -8,6 +9,9 @@ describe('InterestsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        BrowserAnimationsModule,
+      ],
       declarations: [ InterestsComponent ]
     })
     .compileComponents();
--- src/app/resume/resume.component.spec.ts
+++ src/app/resume/resume.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResumeComponent } from './resume.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ResumeComponent', () => {
   let component: ResumeComponent;
@@ -8,6 +9,9 @@ describe('ResumeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        BrowserAnimationsModule,
+      ],
       declarations: [ ResumeComponent ]
     })
     .compileComponents();
